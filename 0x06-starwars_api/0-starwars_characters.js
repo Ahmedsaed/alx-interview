f51cfda5eef092587f773fa8d58698af75e3b4b7
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-01-17 00:37:41 +0200

    Update base url

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (err, response, body) {
   if (err) throw err;
