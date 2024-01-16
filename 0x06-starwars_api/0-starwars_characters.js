a6c1cceea95c9db25cffe9199f18814db03dadfc
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-01-17 00:32:53 +0200

    0x06. Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) throw err;
+  const characters = JSON.parse(body).characters;
+
+  for (const character of characters) {
+    request(character, function (err, response, body) {
+      if (err) throw err;
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
